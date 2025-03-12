357dea24f4667e96afda79db0540ca3488a2d84f
renxiangsan <renxiangsan666@163.com>
2025-03-12 14:41:04 +0800

    *服务号半成品

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const crypto = require("crypto");
 const xml2js = require("xml2js");
+const axios = require("axios");
 const mainModule = require("./main");
+
+// 微信配置参数
+const config = {
+  appId: "wx9c47973f9723b1af", // 请替换为实际AppID
+  appSecret: "9d0d2e0fa9e4720da3fe492b1873a292", // 请替换为实际AppSecret
+  token: "ethan",
+};
+
+// 全局缓存access_token
+let accessToken = {
+  value: "",
+  expires: 0,
+};
 
 // 微信服务器配置的 Token
 const token = "ethan";
@@ -27,6 +41,33 @@ function parseXml(xml) {
   });
 }
 
+// 获取微信access_token
+async function getAccessToken() {
+  const now = Date.now();
+  if (accessToken.value && now < accessToken.expires) {
+    return accessToken.value;
+  }
+
+  try {
+    const url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${config.appId}&secret=${config.appSecret}`;
+    const response = await axios.get(url);
+
+    if (response.data.errcode) {
+      throw new Error(response.data.errmsg);
+    }
+
+    accessToken = {
+      value: response.data.access_token,
+      expires: now + (response.data.expires_in - 300) * 1000, // 提前5分钟刷新
+    };
+
+    return accessToken.value;
+  } catch (error) {
+    console.error("获取access_token失败:", error.message);
+    throw error;
+  }
+}
+
 // 构建回复消息的 XML
 function buildReplyMessage(toUser, fromUser, content) {
   const createTime = Math.floor(Date.now() / 1000);
@@ -40,6 +81,33 @@ function buildReplyMessage(toUser, fromUser, content) {
 }
 
 // 主处理函数
+// 主动推送消息给用户
+async function sendMessageToUser(openId, content) {
+  try {
+    const accessToken = await getAccessToken();
+    const url = `https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=${accessToken}`;
+
+    const message = {
+      touser: openId,
+      msgtype: "text",
+      text: {
+        content: content,
+      },
+    };
+
+    const response = await axios.post(url, message);
+
+    if (response.data.errcode && response.data.errcode !== 0) {
+      throw new Error(response.data.errmsg);
+    }
+
+    return true;
+  } catch (error) {
+    console.error("消息推送失败:", error.message);
+    return false;
+  }
+}
+
 exports.main_handler = async (event, context) => {
   const { queryString, body, httpMethod } = event;
 
@@ -95,4 +163,10 @@ exports.main_handler = async (event, context) => {
     statusCode: 405,
     body: "Method Not Allowed",
   };
+};
+
+// 导出推送消息方法
+module.exports = {
+  ...module.exports,
+  sendMessageToUser,
 };
